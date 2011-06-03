39122197ce7f8175ce6ed3cc80685cb059911998
Alejandro Juarez <juarlex@gmail.com>
2011-06-03 17:24:06 -0500

    Loading helpers and models from environmen.rb

--- environment.rb
+++ environment.rb
@@ -2,3 +2,12 @@ require "rubygems"
 require "bundler"
 Bundler.setup
 Bundler.require(:default)
+
+%w(column_helpers database).each do |name|
+  require "helpers/#{name}"
+end
+
+%w(account address vigency key_chain
+   book_lending book_lending_log user).each do |name|
+  require "models/#{name}"
+end
