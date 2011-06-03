110c36a300a23a67531cce20aaad89239c6f4af6
Alejandro Juarez <juarlex@gmail.com>
2011-06-03 18:38:26 -0500

    Methods to create the different record required by an aleph user

--- models/user.rb
+++ models/user.rb
@@ -9,7 +9,40 @@ class User
                 :location, :settlement, :municipality, :state, :country, :city,
                 :zipcode, :email, :phone, :expiry_date
 
-  validates_presence_of :key, :fullname, :unit, :academic_responsible,
-                        :academic_level, :location, :state, :country, :city,
-                        :zipcode, :email, :phone, :expiry_date
+  validates_presence_of :key, :fullname, :unit, :academic_level, :location,
+                        :country, :city, :zipcode, :email, :phone, :expiry_date
+
+  def initialize(*args)
+    @models = []
+    super *args
+  end
+
+  def add_account
+    @models << Account.new(:key => key, :fullname => fullname, :unit => unit, 
+                           :academic_responsible => academic_responsible,
+                            :academic_level => academic_level)
+  end
+
+  def add_addresses
+    %w(01 02).each do |address_type|
+      @models << Addresses.new(:key => key, :address_type => address_type, :fullname => fullname,
+                               :location => location, :settlement => settlement,
+                               :municipality => municipality, :state => state, 
+                               :city => city, :country => country, :zipcode => zipcode,
+                               :email => email, :phone => phone)
+    end
+  end
+
+  def add_keychain
+    %w(01 00).each do |verification_type|
+        @models << KeyChain.new(:key => key, :verification_type => verfication_type, :password => key)
+    end
+  end
+
+  def add_vigency
+    ['ALEPH', LIBRARY_KEY].each do |suffix|
+        @models << Vigency.new(:key => key, :key_suffix => suffix, :expiry_date => expiry_date,
+                               :unit => unit, :academic_responsible => academic_responsible)
+    end
+  end
 end
