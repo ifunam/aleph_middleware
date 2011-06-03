18e0edcfe934200cc087e39a254bedb2c625acd8
Alejandro Juarez <juarlex@gmail.com>
2011-06-03 17:22:38 -0500

    Removing aleph/* files

--- aleph/book_lending.rb
+++
@@ -1,5 +0,0 @@
-require 'aleph/column_helpers'
-require 'aleph/base'
-
-class BookLending < Sequel::Model(:z36)
-end
--- aleph/book_lending_log.rb
+++
@@ -1,8 +0,0 @@
-require 'aleph/column_helpers'
-require 'aleph/base'
-
-class BookLendingLog < Sequel::Model(:z36h)
-end
-
-#class Unknown < Sequel::Model(:z35)
-#end
