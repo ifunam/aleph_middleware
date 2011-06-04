9af983d5855bf2b1da9e8d704c101a44add2f021
Alejandro Juarez <juarlex@gmail.com>
2011-06-03 21:32:51 -0500

    Basic validations for new records

--- models/user.rb
+++ models/user.rb
@@ -9,23 +9,34 @@ class User
                 :location, :settlement, :municipality, :state, :country, :city,
                 :zipcode, :email, :phone, :expiry_date
 
+  define_attribute_methods [:key, :fullname, :unit, :academic_responsible, :academic_level,
+                :location, :settlement, :municipality, :state, :country, :city,
+                :zipcode, :email, :phone, :expiry_date]
+
   validates_presence_of :key, :fullname, :unit, :academic_level, :location,
                         :country, :city, :zipcode, :email, :phone, :expiry_date
 
-  def initialize(*args)
+  def initialize(attributes={})
     @models = []
-    super *args
+    self.attributes=(attributes)
+    self
   end
 
+   def attributes=(hash)
+      sanitize_for_mass_assignment(hash).each do |attribute, value|
+        send("#{attribute}=", value)
+      end
+   end
+
   def add_account
-    @models << Account.new(:key => key, :fullname => fullname, :unit => unit, 
+    @models << Account.new(:key => key, :fullname => fullname, :unit => unit,
                            :academic_responsible => academic_responsible,
                             :academic_level => academic_level)
   end
 
   def add_addresses
     %w(01 02).each do |address_type|
-      @models << Addresses.new(:key => key, :address_type => address_type, :fullname => fullname,
+      @models << Address.new(:key => key, :address_type => address_type, :fullname => fullname,
                                :location => location, :settlement => settlement,
                                :municipality => municipality, :state => state, 
                                :city => city, :country => country, :zipcode => zipcode,
@@ -35,7 +46,7 @@ class User
 
   def add_keychain
     %w(01 00).each do |verification_type|
-        @models << KeyChain.new(:key => key, :verification_type => verfication_type, :password => key)
+        @models << KeyChain.new(:key => key, :verification_type => verification_type, :password => key)
     end
   end
 
@@ -44,5 +55,30 @@ class User
         @models << Vigency.new(:key => key, :key_suffix => suffix, :expiry_date => expiry_date,
                                :unit => unit, :academic_responsible => academic_responsible)
     end
+  end
+
+  def setup_models
+    add_account
+    add_addresses
+    add_keychain
+    add_vigency
+  end
+
+  def valid?
+    setup_models
+    if super
+      models_valid?
+    else
+      return false
+    end
+  end
+
+  def models_valid?
+    @models.each do |model|
+      unless model.valid?
+        errors.add model.class.name.downcase.to_sym, model.errors.full_messages.join(', ')
+      end
+    end
+    errors.count == 0
   end
 end
