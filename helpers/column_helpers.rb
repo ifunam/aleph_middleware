de11e9c01d358ba4faadafdc0cb686b13e96f1a4
Alejandro Juarez <juarlex@gmail.com>
2011-06-06 18:57:58 -0500

    Minor bug fix for column_helpers

--- helpers/column_helpers.rb
+++ helpers/column_helpers.rb
@@ -2,14 +2,15 @@ module Aleph
   module ColumnHelpers
 
     def blank_spaces_as_suffix(string,length=12)
+      string = string.to_s
       if string.size < length
-        string << fill_with_blank_spaces(string, length)
+        string << fill_with_blank_spaces(string, length).to_s
       end
       truncate(string,length)
     end
 
     def fill_with_blank_spaces(string, length)
-       ' '  * (length - string.size)
+      ' '  * (length - string.to_s.size)
     end
 
     def truncate(string, length)
