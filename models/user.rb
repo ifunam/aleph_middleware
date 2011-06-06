1aa76f9dfee89169c656c46437b6017baf62d6f4
Alejandro Juarez <juarlex@gmail.com>
2011-06-06 18:03:42 -0500

    save implementation for models/user

--- models/user.rb
+++ models/user.rb
@@ -6,66 +6,84 @@ class User
 
   attr_accessor :key, :fullname, :unit, :academic_responsible, :academic_level,
                 :location, :settlement, :municipality, :state, :country, :city,
-                :zipcode, :email, :phone, :expiry_date
+                :zipcode, :email, :phone, :expiry_date, :new_record
 
   validates_presence_of :key, :fullname, :unit, :academic_level, :location,
                         :country, :city, :zipcode, :email, :phone, :expiry_date
 
   def initialize(attributes={})
     @models = []
-    self.attributes=(attributes)
+    self.attributes = attributes
+    self.new_record = attributes[:new_record] || true
     self
   end
 
-   def attributes=(hash)
-      sanitize_for_mass_assignment(hash).each do |attribute, value|
-        send("#{attribute}=", value)
-      end
-   end
-
-  def add_account
-    @models << Account.new(:key => key, :fullname => fullname, :unit => unit,
-                           :academic_responsible => academic_responsible,
-                           :academic_level => academic_level)
-  end
-
-  def add_addresses
-    %w(01 02).each do |address_type|
-      @models << Address.new(:key => key, :address_type => address_type, :fullname => fullname,
-                             :location => location, :settlement => settlement,
-                             :municipality => municipality, :state => state, 
-                             :city => city, :country => country, :zipcode => zipcode,
-                             :email => email, :phone => phone)
-    end
-  end
-
-  def add_keychain
-    %w(01 00).each do |verification_type|
-        @models << KeyChain.new(:key => key, :verification_type => verification_type, :password => key)
-    end
-  end
-
-  def add_vigency
-    ['ALEPH', LIBRARY_KEY].each do |suffix|
-        @models << Vigency.new(:key => key, :key_suffix => suffix, :expiry_date => expiry_date,
-                               :unit => unit, :academic_responsible => academic_responsible)
-    end
-  end
-
-  def setup_models
-    add_account
-    add_addresses
-    add_keychain
-    add_vigency
-  end
-
   def valid?
-    setup_models
+    fill_models if @models.empty?
     if super
       models_valid?
     else
       return false
     end
+  end
+
+  def save
+    if valid? and new_record?
+      save_models
+    else
+      return false
+    end
+  end
+
+  private
+
+  def attributes=(hash)
+    sanitize_for_mass_assignment(hash).each do |attribute, value|
+      send("#{attribute}=", value)
+    end
+  end
+
+  def account_attributes
+    { :fullname => fullname, :unit => unit, :academic_responsible => academic_responsible,
+      :academic_level => academic_level, :key => key
+    }
+  end
+
+  def new_account
+    Account.new account_attributes
+  end
+
+  def address_attributes
+    { :key => key, :fullname => fullname, :location => location, :settlement => settlement,
+      :municipality => municipality, :state => state, :city => city, :country => country,
+      :zipcode => zipcode, :email => email, :phone => phone
+    }
+  end
+
+  def new_address(address_type)
+    Address.new address_attributes.merge(:address_type => address_type)
+  end
+
+  def keychain_attributes
+    { :key => key, :password => key }
+  end
+
+  def new_keychain(verification_type)
+    KeyChain.new keychain_attributes.merge(:verification_type => verification_type)
+  end
+
+  def vigency_attributes
+    { :key => key, :expiry_date => expiry_date,
+      :unit => unit, :academic_responsible => academic_responsible
+    }
+  end
+
+  def new_vigency(suffix_key)
+    Vigency.new vigency_attributes.merge(:key_suffix => suffix_key)
+  end
+
+  def new_record?
+    @new_record
   end
 
   def models_valid?
@@ -74,6 +92,32 @@ class User
         errors.add model.class.name.downcase.to_sym, model.errors.full_messages.join(', ')
       end
     end
-    errors.count == 0
+    !has_errors?
+  end
+
+  def has_errors?
+    errors.count > 0
+  end
+
+  def save_models
+    @models.each do |model|
+      record_saved = model.save
+      return false unless record_saved
+    end
+  end
+
+  def fill_models
+    @models.clear
+    if new_record?
+      @models << new_account
+      %w(01 02).each do |address_type| @models << new_address(address_type) end
+      %w(01 00).each do |verification_type| @models << new_keychain(verification_type) end
+      ['ALEPH', LIBRARY_KEY].each do |suffix_key| @models << new_vigency(suffix_key) end
+    else
+      @models << Account.first_by_key(key)
+      Address.all_by_key(key).each do |address| @models << address end
+      KeyChain.all_by_key(key).each do |keychain| @models << keychain end
+      Vigency.all_by_key(key).each do |vigency| @models << keychain end
+    end
   end
 end
