004342e29cde0c217831bb3494990b70602d2193
Alejandro Juarez <juarlex@gmail.com>
2011-06-07 17:59:55 -0500

    create_token method added to models/client.rb and minor changes

--- models/client.rb
+++ models/client.rb
@@ -1,8 +1,20 @@
+require 'digest/sha2'
 class Client < Sequel::Model(MIDDLEWARE_DB[:clients])
   plugin :validation_helpers
-  def validate
-    validate_presence [:ip_address, :emails, :enabled, :name]
-    validates_uniqueness :name
-  end
   one_to_many :transactions
+
+  def before_create
+    self.token ||= create_token
+    super
+  end
+
+  def validate
+    validates_presence [:ip_address, :emails, :enabled, :name]
+    validates_unique :name
+  end
+
+  private
+  def create_token
+    Digest::SHA2.hexdigest(Time.now.to_s)
+  end
 end
