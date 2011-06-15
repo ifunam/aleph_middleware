f327f40aa7784d1cb227bc48ecb2406ffa9a0569
Alejandro Juarez <juarlex@gmail.com>
2011-06-15 18:50:13 -0500

    minor changes for capistrano files

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,13 +1,16 @@
+require "bundler/capistrano"
 set :application, "aleph_middleware"
-set :domain, "alephsucks.fisica.unam.mx"
+set :domain, "132.248.7.233"
+set :scm, :git
 set :repository,  "git://github.com/ifunam/aleph_middleware.git"
 
 set :deploy_to, "/var/sinatra/#{application}"
-default_run_options[:pty] = true
 set :use_sudo, false
 set :user, "deployer"
 
-set :scm, :git
+ssh_options[:forward_agent] = true
+default_run_options[:pty] = true
+
 
 namespace :deploy do
 
@@ -15,4 +18,7 @@ namespace :deploy do
     run "cd #{deploy_to}/current && mkdir -p tmp && touch tmp/restart.txt"
   end
 
+  task :update_code do
+
+  end
 end
