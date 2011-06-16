245cd2fb1b78d41404ad0d43b992245ce77419cf
Alejandro Juarez <juarlex@gmail.com>
2011-06-15 23:14:47 -0500

    minor changes

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,20 +3,15 @@ set :application, "aleph_middleware"
 set :domain, "132.248.7.233"
 set :use_sudo, false
 set :user, "deployer"
+set :deploy_to, "/var/sinatra/#{application}"
 
 set :scm, :git
 set :repository,  "git://github.com/ifunam/aleph_middleware.git"
+set :branch, "master"
 
-set :deploy_to, "/var/sinatra/#{application}"
-
-ssh_options[:forward_agent] = true
 default_run_options[:pty] = true
-
-#set :app, domain
-#set :web, domain
-#set :db, domain
-
 server domain, :app, :web, :db, :primary => true
+
 namespace :deploy do
 
   task :restart do
@@ -25,6 +20,6 @@ namespace :deploy do
   end
 
   task :update_code do
-   run "ln -nfs #{deploy_to}/share/aleph_middleware/config.yml #{release_path}/config.yml"
+    run "ln -nfs #{deploy_to}/shared/config.yml #{release_path}/config.yml"
   end
 end
