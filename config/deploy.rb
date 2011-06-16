1eeaf90d575dd700633e1371c99e2337cb994223
Alejandro Juarez <juarlex@gmail.com>
2011-06-16 09:57:30 -0500

    update task for capistrano added

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,9 +4,11 @@ set :domain, "132.248.7.233"
 set :use_sudo, false
 set :user, "deployer"
 set :deploy_to, "/var/sinatra/#{application}"
+set :deploy_via, :remote_cache
 
 set :scm, :git
-set :repository,  "git@github.com:ifunam/aleph_middleware.git"
+set :git_shallow_clone,1
+set :repository,  "git://github.com/ifunam/aleph_middleware.git"
 set :branch, "master"
 
 default_run_options[:pty] = true
@@ -14,12 +16,17 @@ server domain, :app, :web, :db, :primary => true
 
 namespace :deploy do
 
-  task :restart do
-    run "mkdir -p #{current_path}/tmp"
-    run "touch #{current_path}/tmp/restart.txt"
+  task :update do
+    revision_number = Time.now.strftime("%Y%m%d%H%m%S")
+    run "git clone #{repository} #{deploy_to}/releases/#{revision_number}"
+    run "mkdir -p  #{deploy_to}/releases/#{revision_number}/tmp"
+    run "mkdir -p  #{deploy_to}/releases/#{revision_number}/public"
+    run "ln -nsf #{deploy_to}/releases/#{revision_number}  #{deploy_to}/current"
+    run "ln -nfs #{deploy_to}/shared/system/config.yml  #{deploy_to}/current/config.yml"
   end
 
-  task :update_code do
-    run "ln -nfs #{deploy_to}/shared/config.yml #{release_path}/config.yml"
+  task :restart do
+   run "touch #{current_path}/tmp/restart.txt"
   end
+
 end
